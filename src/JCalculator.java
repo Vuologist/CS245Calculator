77008f1bdc09f3de8bbc97ed437aad5f84c72f7f
Anthony Vu <anthony.vu2142@gmail.com>
2018-01-30 22:21:04 -0800

    added code :)

--- src/JCalculator.java
+++ src/JCalculator.java
@@ -14,24 +14,31 @@ import javax.swing.*;
 public class JCalculator implements ActionListener {
 
     JButton button[] = new JButton[16];
+    JLabel display = new JLabel();
 
     public JCalculator(){
         JFrame jfrm = new JFrame("Calculator");
         jfrm.setLayout(new GridLayout(4,4));
-        jfrm.setSize(700,700);
+        jfrm.setSize(400,400);
         jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jfrm.setVisible(true);
 
+        String[] buttonSym = {"7","8","9","/",
+                            "4","5","6","*",
+                            "1","2","3","-",
+                            "c","0","=","+"};
+
+        display = new JLabel("something");
+        display.setLayout(new GridLayout(1,4));
+        display.setHorizontalAlignment(JLabel.RIGHT);
+        jfrm.add(display,BorderLayout.NORTH);
+
         for(int i = 0; i < button.length; i++) {
-            //button[i] =
+            button[i] = new JButton(buttonSym[i]);
+            button[i].addActionListener(this);
+            jfrm.add(button[i]);
         }
-
-
     }
-
-
-
-
 
     public void actionPerformed(ActionEvent ae) {
 
